07cba7919e494c33c9c576c654743258b907911c
Gus <Gus@Michelles-MacBook-Pro.local>
2022-10-14 17:35:01 +0800

    Edited Background image component & working as intended

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
+import Background from './components/background';
 
 function App() {
   return (
-    <main>
-    </main>
+    <Background></Background>
   );
 }
 
---
+++ src/components/background.js
@@ -0,0 +1,14 @@
+import React from "react"
+// Background Image of Sky
+
+const Background = () => {
+    return (
+        <div className="background">
+            {/* background image from "https://www.vecteezy.com/free-vector/nature">Nature Vectors by Vecteezy */}
+            <img id="background-image" src="./images/nature-background.png" alt="2D nature background"/>
+        </div>
+    )
+}
+
+// Export
+export default Background
